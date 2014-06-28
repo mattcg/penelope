4dd223369ce967c0d0df627bf0e45a52122f0194
Matthew Caruana Galizia <mattcg@gmail.com>
2014-06-27 19:22:38 -0600

    Add support for long text format

--- themes/default/types/text.php
+++ themes/default/types/text.php
@@ -1,9 +1,15 @@
 <?php
 
+if ($property->getSchema()->getOption('format.long')) {
+	$format = 'long';
+} else {
+	$format = 'short';
+}
+
 if ($property->getSchema()->isMultiValue()) {
 
 ?>
-<ul class="multivalue">
+<ul class="multivalue format-<?php __class($format); ?>">
 	<?php
 
 	foreach ($property->getValue() as $value) {
@@ -20,7 +26,9 @@ if ($property->getSchema()->isMultiValue()) {
 
 } else {
 
-	__($property->getValue());
+?>
+<div class="format-<?php __class($format); ?>"><?php __($property->getValue()); ?></div>
+<?php
 
 }
 
--- themes/default/types/text_input.php
+++ themes/default/types/text_input.php
@@ -2,18 +2,34 @@
 
 if ($property->getSchema()->isMultiValue()) {
 	foreach (array_merge($property->getValue(), array(null)) as $value) {
+		if ($property->getSchema()->getOption('format.long')) {
+
+?>
+<textarea id="<?php __($property_id); ?>" name="<?php __($property->getName()); ?>"><?php __($value); ?></textarea>
+<?php
+
+		} else {
 
 ?>
 <input id="<?php __($property_id); ?>" name="<?php __($property->getName()); ?>"<?php if ($value) { ?> value="<?php __($value); ?>"<?php } ?>>
 <?php
 
+		}
 	}
 } else {
+	if ($property->getSchema()->getOption('format.long')) {
+
+?>
+<textarea id="<?php __($property_id); ?>" name="<?php __($property->getName()); ?>"><?php __($property->getValue()); ?></textarea>
+<?php
+
+	} else {
 
 ?>
 <input id="<?php __($property_id); ?>" name="<?php __($property->getName()); ?>"<?php if ($property->getValue()) { ?> value="<?php __($property->getValue()); ?>"<?php } ?>>
 <?php
 
+	}
 }
 
 ?>
