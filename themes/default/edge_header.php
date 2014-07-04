069d67c1e32d2bbc25f61ba023feaa2f41d5b3a1
Matthew Caruana Galizia <mattcg@gmail.com>
2014-07-04 12:28:34 -0600

    Add link to node

--- themes/default/edge_header.php
+++ themes/default/edge_header.php
@@ -13,6 +13,7 @@
 			?>
 			<li class="new"><a class="new" href="<?php __($node->getNewEdgePath($edge_schema)); ?>" title="Create a new <?php __($schema_name); ?> relationship from <?php __($node->getTitle()); ?>">New</a></li>
 			<li class="collection"><a class="collection" href="<?php __($node->getEdgeCollectionPath($edge_schema)); ?>" title="<?php __($schema_name); ?> list">View all</a></li>
+			<li class="view-parent"><a class="view-parent" href="<?php __($node->getPath()); ?>" title="<?php __($node->getTitle()); ?>">Back to <?php __($node->getSchema()->getName()); ?></a></li>
 		</ul>
 	</nav>
 </header>
