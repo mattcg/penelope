16069305f6ea1481a06ac52c0326c75af479f786
Matthew Caruana Galizia <mattcg@gmail.com>
2014-07-06 21:38:11 -0600

    Always save the relationship

--- Penelope/Edge.php
+++ Penelope/Edge.php
@@ -132,10 +132,8 @@ class Edge extends Object {
 		}
 
 		$edge = $this->object;
-		if ($this->from_node and $this->to_node) {
-			$edge->setStartNode($this->from_node->getClientObject());
-			$edge->setEndNode($this->to_node->getClientObject());
-		}
+		$edge->setStartNode($this->from_node->getClientObject());
+		$edge->setEndNode($this->to_node->getClientObject());
 
 		parent::save();
 	}
