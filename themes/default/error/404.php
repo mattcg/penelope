03fd30e6f239031d6af0e06591261103366aa954
Matthew Caruana Galizia <mattcg@gmail.com>
2014-07-10 17:27:30 -0600

    404 template should only display error message in debug mode

--- themes/default/error/404.php
+++ themes/default/error/404.php
@@ -1,6 +1,24 @@
 <main class="error error-404">
 	<div class="body">
 		<h1>Not Found</h1>
-		<p><?php if (!empty($error)) echo $error->getMessage(); else 'The requested object was not found.'; ?></p>
+		<?php
+
+		global $app;
+
+		if (!empty($error) and $app->config('debug')) {
+
+		?>
+		<p><?php __(_e($error->getMessage())); ?></p>
+		<?php
+
+		} else {
+
+		?>
+		<p>The requested object was not found.</p>
+		<?php
+
+		}
+
+		?>
 	</div>
 </main>
