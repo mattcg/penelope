5387e9937f0a2f12fe6fa0e8f25ab4de883aac55
Matthew Caruana Galizia <mattcg@gmail.com>
2014-07-10 23:21:32 -0600

    Button style refresh

--- themes/default/edge_header.php
+++ themes/default/edge_header.php
@@ -11,21 +11,21 @@
 			$schema_name = $edge_schema->getName();
 
 			?>
-			<li class="new"><a class="new" href="<?php __($node->getNewEdgePath($edge_schema)); ?>" title="<?php __(_m('new_edge_title', $schema_name, $node->getTitle())); ?>"><?php __(_m('new_edge_link_text')); ?></a></li>
+			<li class="new"><a class="button new" href="<?php __($node->getNewEdgePath($edge_schema)); ?>" title="<?php __(_m('new_edge_title', $schema_name, $node->getTitle())); ?>"><?php __(_m('new_edge_link_text')); ?></a></li>
 			<?php
 
 			if (isset($edge)) {
 
 			?>
-			<li class="edit"><a class="edit" href="<?php __($edge->getEditPath()); ?>" title="<?php __(_m('edit_edge_title', $edge->getTitle())); ?>"><?php __(_m('edit_edge_link_text')); ?></a></li>
+			<li class="edit"><a class="button edit" href="<?php __($edge->getEditPath()); ?>" title="<?php __(_m('edit_edge_title', $edge->getTitle())); ?>"><?php __(_m('edit_edge_link_text')); ?></a></li>
 			<li class="view"><a class="view" href="<?php __($edge->getPath()); ?>" title="<?php __(_m('view_edge_title')); ?>"><?php __(_m('view_edge_link_text')); ?></a></li>
 			<?php
 
 			}
 
 			?>
-			<li class="collection"><a class="collection" href="<?php __($node->getEdgeCollectionPath($edge_schema)); ?>" title="<?php __(_m('view_edge_collection_title', $schema_name)); ?>"><?php __(_m('view_edge_collection_link_text')); ?></a></li>
-			<li class="view-parent"><a class="view-parent" href="<?php __($node->getPath()); ?>" title="<?php __(_m('edge_parent_link_title', $node->getTitle())); ?>"><?php __(_m('edge_parent_link_text', $node->getSchema()->getName())); ?></a></li>
+			<li class="collection"><a class="button collection" href="<?php __($node->getEdgeCollectionPath($edge_schema)); ?>" title="<?php __(_m('view_edge_collection_title', $schema_name)); ?>"><?php __(_m('view_edge_collection_link_text')); ?></a></li>
+			<li class="view-parent"><a class="button view-parent" href="<?php __($node->getPath()); ?>" title="<?php __(_m('edge_parent_link_title', $node->getTitle())); ?>"><?php __(_m('edge_parent_link_text', $node->getSchema()->getName())); ?></a></li>
 		</ul>
 	</nav>
 </header>
--- themes/default/node_header.php
+++ themes/default/node_header.php
@@ -11,20 +11,20 @@
 			$schema_name = $node_schema->getName();
 
 			?>
-			<li class="new"><a class="new" href="<?php __($node_schema->getNewPath()); ?>" title="Create a new <?php __($schema_name); ?>">New</a></li>
+			<li class="new"><a class="button new" href="<?php __($node_schema->getNewPath()); ?>" title="Create a new <?php __($schema_name); ?>">New</a></li>
 			<?php
 
 			if (isset($node)) {
 
 			?>
-			<li class="edit"><a class="edit" href="<?php __($node->getEditPath()); ?>" title="Edit <?php __($node->getTitle()); ?>">Edit</a></li>
-			<li class="view"><a class="view" href="<?php __($node->getPath()); ?>" title="View <?php __($node->getTitle()); ?>">View</a></li>
+			<li class="edit"><a class="button edit" href="<?php __($node->getEditPath()); ?>" title="Edit <?php __($node->getTitle()); ?>">Edit</a></li>
+			<li class="view"><a class="button view" href="<?php __($node->getPath()); ?>" title="View <?php __($node->getTitle()); ?>">View</a></li>
 			<?php
 
 			}
 
 			?>
-			<li class="collection"><a class="collection" href="<?php __($node_schema->getCollectionPath()); ?>" title="<?php __($schema_name); ?> list">View all</a></li>
+			<li class="collection"><a class="button collection" href="<?php __($node_schema->getCollectionPath()); ?>" title="<?php __($schema_name); ?> list">View all</a></li>
 		</ul>
 	</nav>
 </header>
