84860df935e02cfc8556fe9add90aaae76e89963
Matthew Caruana Galizia <mattcg@gmail.com>
2014-07-11 14:23:42 -0600

    Sort by name

--- Penelope/Controllers/NodesController.php
+++ Penelope/Controllers/NodesController.php
@@ -24,6 +24,11 @@ class NodesController extends ObjectController {
 		$view_data = array('title' => $this->_m('node_collection_title', $node_schema->getName()), 'node_schema' => $node_schema);
 		$view_data['nodes'] = $node_schema->getCollection($this->client);
 
+		// Sort by name.
+		usort($view_data['nodes'], function($a, $b) {
+			return strcmp($a->getTitle(), $b->getTitle());
+		});
+
 		$this->app->render('nodes', $view_data);
 	}
 
