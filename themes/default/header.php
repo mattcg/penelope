a60e81668b8c6ba080dc0296dba4804f8a3a920d
Matthew Caruana Galizia <mattcg@gmail.com>
2014-07-15 20:18:49 -0600

    Not ready for mobile yet

--- themes/default/header.php
+++ themes/default/header.php
@@ -4,7 +4,6 @@
 		<meta charset="utf-8">
 		<meta name="generator" content="Penelope v<?php __(Karwana\Penelope\VERSION); ?>">
 		<title><?php __(_e(strip_tags($title))); ?></title>
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 		<?php
 
 		foreach ($resources['css'] as $css_url) {
