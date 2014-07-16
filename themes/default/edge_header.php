294e2f73bada8a09cbd44d2fbbc5ee20136e9b50
Matthew Caruana Galizia <mattcg@gmail.com>
2014-07-15 19:46:12 -0600

    Strip tags from messages in title attributes

--- themes/default/edge_header.php
+++ themes/default/edge_header.php
@@ -11,21 +11,21 @@
 			$schema_name = $edge_schema->getName();
 
 			?>
-			<li class="new"><a class="button new" href="<?php __($node->getNewEdgePath($edge_schema)); ?>" title="<?php __(_m('new_edge_button_title', $schema_name, $node->getTitle())); ?>"><?php __(_m('new_edge_button_text')); ?></a></li>
+			<li class="new"><a class="button new" href="<?php __($node->getNewEdgePath($edge_schema)); ?>" title="<?php __(_a('new_edge_button_title', $schema_name, $node->getTitle())); ?>"><?php __(_m('new_edge_button_text')); ?></a></li>
 			<?php
 
 			if (isset($edge)) {
 
 			?>
-			<li class="edit"><a class="button edit" href="<?php __($edge->getEditPath()); ?>" title="<?php __(_m('edit_edge_button_title', $edge->getTitle())); ?>"><?php __(_m('edit_edge_button_text')); ?></a></li>
-			<li class="view"><a class="view" href="<?php __($edge->getPath()); ?>" title="<?php __(_m('view_edge_button_title')); ?>"><?php __(_m('view_edge_button_text')); ?></a></li>
+			<li class="edit"><a class="button edit" href="<?php __($edge->getEditPath()); ?>" title="<?php __(_a('edit_edge_button_title', $edge->getTitle())); ?>"><?php __(_m('edit_edge_button_text')); ?></a></li>
+			<li class="view"><a class="view" href="<?php __($edge->getPath()); ?>" title="<?php __(_a('view_edge_button_title')); ?>"><?php __(_m('view_edge_button_text')); ?></a></li>
 			<?php
 
 			}
 
 			?>
-			<li class="collection"><a class="button collection" href="<?php __($node->getEdgeCollectionPath($edge_schema)); ?>" title="<?php __(_m('view_edge_collection_button_title', $schema_name)); ?>"><?php __(_m('view_edge_collection_button_text')); ?></a></li>
-			<li class="view-parent"><a class="button view-parent" href="<?php __($node->getPath()); ?>" title="<?php __(_m('edge_parent_button_title', $node->getTitle())); ?>"><?php __(_m('edge_parent_button_text', $node->getSchema()->getName())); ?></a></li>
+			<li class="collection"><a class="button collection" href="<?php __($node->getEdgeCollectionPath($edge_schema)); ?>" title="<?php __(_a('view_edge_collection_button_title', $schema_name)); ?>"><?php __(_m('view_edge_collection_button_text')); ?></a></li>
+			<li class="view-parent"><a class="button view-parent" href="<?php __($node->getPath()); ?>" title="<?php __(_a('edge_parent_button_title', $node->getTitle())); ?>"><?php __(_m('edge_parent_button_text', $node->getSchema()->getName())); ?></a></li>
 		</ul>
 	</nav>
 </header>
--- themes/default/node_header.php
+++ themes/default/node_header.php
@@ -11,20 +11,20 @@
 			$schema_name = $node_schema->getName();
 
 			?>
-			<li class="new"><a class="button new" href="<?php __($node_schema->getNewPath()); ?>" title="<?php __(_m('new_node_button_title', $schema_name)); ?>"><?php __(_m('new_node_button_text')) ?></a></li>
+			<li class="new"><a class="button new" href="<?php __($node_schema->getNewPath()); ?>" title="<?php __(_a('new_node_button_title', $schema_name)); ?>"><?php __(_m('new_node_button_text')) ?></a></li>
 			<?php
 
 			if (isset($node)) {
 
 			?>
-			<li class="edit"><a class="button edit" href="<?php __($node->getEditPath()); ?>" title="<?php __(_m('edit_node_button_title', $node->getTitle())); ?>"><?php __(_m('edit_node_button_text')); ?></a></li>
-			<li class="view"><a class="button view" href="<?php __($node->getPath()); ?>" title="<?php __(_m('view_node_button_title', $node->getTitle())); ?>"><?php __(_m('view_node_button_text')); ?></a></li>
+			<li class="edit"><a class="button edit" href="<?php __($node->getEditPath()); ?>" title="<?php __(_a('edit_node_button_title', $node->getTitle())); ?>"><?php __(_m('edit_node_button_text')); ?></a></li>
+			<li class="view"><a class="button view" href="<?php __($node->getPath()); ?>" title="<?php __(_a('view_node_button_title', $node->getTitle())); ?>"><?php __(_m('view_node_button_text')); ?></a></li>
 			<?php
 
 			}
 
 			?>
-			<li class="collection"><a class="button collection" href="<?php __($node_schema->getCollectionPath()); ?>" title="<?php __(_m('view_node_collection_button_title', $schema_name)); ?>"><?php __(_m('view_node_collection_button_text')); ?></a></li>
+			<li class="collection"><a class="button collection" href="<?php __($node_schema->getCollectionPath()); ?>" title="<?php __(_a('view_node_collection_button_title', $schema_name)); ?>"><?php __(_m('view_node_collection_button_text')); ?></a></li>
 		</ul>
 	</nav>
 </header>
