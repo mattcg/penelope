dc430f3299715d3cc369ed48fac95b5f65c54c41
Matthew Caruana Galizia <mattcg@gmail.com>
2014-11-27 14:22:38 -0600

    Add optional document classes to html tag

--- themes/default/header.php
+++ themes/default/header.php
@@ -1,5 +1,19 @@
 <!DOCTYPE html>
-<html lang="<?php __($locale); ?>">
+<html lang="<?php __($locale); ?>"<?php
+
+	if (isset($document_classes)) {
+
+	?> class="<?php
+
+	foreach ($document_classes as $document_class) {
+		__class($document_class);
+	}
+
+	?>"<?php
+
+	}
+
+	?>>
 	<?php
 
 	require __path('header_head.php');
