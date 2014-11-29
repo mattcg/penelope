748f803c15612a0909856e935e3e3011920cce85
Matthew Caruana Galizia <mattcg@gmail.com>
2014-11-29 11:39:00 -0600

    Use date and datetime input types as strtotime cannot handle non-English date strings

--- themes/default/types/datetime_input.php
+++ themes/default/types/datetime_input.php
@@ -4,19 +4,19 @@ if ($property->getSchema()->isMultiValue()) {
 	foreach ((array) $property->getValue() as $value) {
 
 ?>
-<input name="<?php __(_e($property->getName())); ?>[]"<?php if ($value) { ?> value="<?php __datetime($value); ?>"<?php } ?>>
+<input type="datetime" name="<?php __(_e($property->getName())); ?>[]"<?php if ($value) { ?> value="<?php __(date('Y-m-d\TH:i:sP', $value)); ?>"<?php } ?>>
 <?php
 
 	}
 
 ?>
-<input id="<?php __($property_id); ?>" name="<?php __(_e($property->getName())); ?>[]" class="new">
+<input type="datetime" id="<?php __($property_id); ?>" name="<?php __(_e($property->getName())); ?>[]" class="new">
 <?php
 
 } else {
 
 ?>
-<input id="<?php __($property_id); ?>" name="<?php __(_e($property->getName())); ?>"<?php if ($property->hasValue()) { ?> value="<?php __datetime($property->getValue()); ?>"<?php } ?>>
+<input type="datetime" id="<?php __($property_id); ?>" name="<?php __(_e($property->getName())); ?>"<?php if ($property->hasValue()) { ?> value="<?php __(date('Y-m-d\TH:i:sP', $property->getValue())); ?>"<?php } ?>>
 <?php
 
 }
