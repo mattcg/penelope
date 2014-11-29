85c157d9e8dda5a1e204f06528dc4eadcec426ae
Matthew Caruana Galizia <mattcg@gmail.com>
2014-11-29 11:39:00 -0600

    Use datetime inputs

--- themes/default/types/date_input.php
+++ themes/default/types/date_input.php
@@ -4,19 +4,19 @@ if ($property->getSchema()->isMultiValue()) {
 	foreach ((array) $property->getValue() as $value) {
 
 ?>
-<input name="<?php __(_e($property->getName())); ?>[]"<?php if ($value) { ?> value="<?php __date($value); ?>"<?php } ?>>
+<input type="date" name="<?php __(_e($property->getName())); ?>[]"<?php if ($value) { ?> value="<?php __(date('Y-m-d', $value)); ?>"<?php } ?>>
 <?php
 
 	}
 
 ?>
-<input id="<?php __($property_id); ?>" name="<?php __(_e($property->getName())); ?>[]" class="new">
+<input type="date" id="<?php __($property_id); ?>" name="<?php __(_e($property->getName())); ?>[]" class="new">
 <?php
 
 } else {
 
 ?>
-<input id="<?php __($property_id); ?>" name="<?php __(_e($property->getName())); ?>"<?php if ($property->hasValue()) { ?> value="<?php __date($property->getValue()); ?>"<?php } ?>>
+<input type="date" id="<?php __($property_id); ?>" name="<?php __(_e($property->getName())); ?>"<?php if ($property->hasValue()) { ?> value="<?php __(date('Y-m-d', $property->getValue())); ?>"<?php } ?>>
 <?php
 
 }
