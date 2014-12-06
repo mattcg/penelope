8bdeacb7f418921363b3194b328413a02eb72523
Matthew Caruana Galizia <mattcg@gmail.com>
2014-12-06 02:40:00 -0600

    Improve Datetime test coverage

--- tests/Penelope/Types/DatetimeTest.php
+++ tests/Penelope/Types/DatetimeTest.php
@@ -10,8 +10,9 @@
  * @since      File available since Release 1.0.0
  */
 
+namespace Karwana\Penelope\Tests;
+
 use Karwana\Penelope\Types\Datetime;
-use Karwana\Penelope\Exceptions\TypeException;
 
 class DatetimeTest extends \PHPUnit_Framework_TestCase {
 
@@ -25,8 +26,23 @@ class DatetimeTest extends \PHPUnit_Framework_TestCase {
 		Datetime::unserialize('0');
 	}
 
+	public function testUnserialize_handlesIntegers() {
+		$this->assertEquals(1399161600, Datetime::unserialize(1399161600));
+	}
+
 	public function testUnserialize_handlesDates() {
 		$this->assertEquals(-904348800, Datetime::unserialize('05/06/1941'));
 		$this->assertEquals(1399161600, Datetime::unserialize('4 May 2014'));
+	}
+
+	public function testIsValid_returnsTrueForEmpty() {
+		$this->assertTrue(Datetime::isValid(null));
+		$this->assertTrue(Datetime::isValid(0));
+		$this->assertTrue(Datetime::isValid(''));
+	}
+
+	public function testIsValid_returnsFalseForNonEmptyNonInteger() {
+		$this->assertFalse(Datetime::isValid('1399161600', null, $message));
+		$this->assertEquals('Invalid type received.', $message);
 	}
 }
