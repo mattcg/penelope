4851c41ab94cb94aaaa8bf378dfabc2ad5cac03c
Matthew Caruana Galizia <mattcg@gmail.com>
2014-12-12 13:44:35 -0600

    Fix edge header button classes

--- themes/default/edge_header.php
+++ themes/default/edge_header.php
@@ -35,21 +35,21 @@
 	</div>
 	<nav class="crud">
 		<ul>
-			<li class="new"><a class="button new" href="<?php __(_e($node->getNewEdgePath($edge_schema))); ?>" title="<?php __(_a('new_edge_button_title', $edge_schema->getDisplayName(), $node->getTitle())); ?>"><?php __(_m('new_edge_button_text')); ?></a></li>
+			<li class="new"><a class="crud-button new" href="<?php __(_e($node->getNewEdgePath($edge_schema))); ?>" title="<?php __(_a('new_edge_button_title', $edge_schema->getDisplayName(), $node->getTitle())); ?>"><?php __(_m('new_edge_button_text')); ?></a></li>
 			<?php
 
 			if (isset($edge)) {
 
 			?>
-			<li class="edit"><a class="button edit" href="<?php __(_e($edge->getEditPath())); ?>" title="<?php __(_a('edit_edge_button_title', $edge->getTitle())); ?>"><?php __(_m('edit_edge_button_text')); ?></a></li>
-			<li class="view"><a class="button view" href="<?php __(_e($edge->getPath())); ?>" title="<?php __(_a('view_edge_button_title')); ?>"><?php __(_m('view_edge_button_text')); ?></a></li>
+			<li class="edit"><a class="crud-button edit" href="<?php __(_e($edge->getEditPath())); ?>" title="<?php __(_a('edit_edge_button_title', $edge->getTitle())); ?>"><?php __(_m('edit_edge_button_text')); ?></a></li>
+			<li class="view"><a class="crud-button view" href="<?php __(_e($edge->getPath())); ?>" title="<?php __(_a('view_edge_button_title')); ?>"><?php __(_m('view_edge_button_text')); ?></a></li>
 			<?php
 
 			}
 
 			?>
-			<li class="collection"><a class="button collection" href="<?php __(_e($node->getEdgeCollectionPath($edge_schema))); ?>" title="<?php __(_a('view_edge_collection_button_title', $edge_schema->getDisplayName())); ?>"><?php __(_m('view_edge_collection_button_text')); ?></a></li>
-			<li class="view-parent"><a class="button view-parent" href="<?php __(_e($node->getPath())); ?>" title="<?php __(_a('edge_parent_button_title', $node->getTitle())); ?>"><?php __(_m('edge_parent_button_text', $node->getSchema()->getDisplayName())); ?></a></li>
+			<li class="collection"><a class="crud-button collection" href="<?php __(_e($node->getEdgeCollectionPath($edge_schema))); ?>" title="<?php __(_a('view_edge_collection_button_title', $edge_schema->getDisplayName())); ?>"><?php __(_m('view_edge_collection_button_text')); ?></a></li>
+			<li class="view-parent"><a class="crud-button view-parent" href="<?php __(_e($node->getPath())); ?>" title="<?php __(_a('edge_parent_button_title', $node->getTitle())); ?>"><?php __(_m('edge_parent_button_text', $node->getSchema()->getDisplayName())); ?></a></li>
 		</ul>
 	</nav>
 </header>
