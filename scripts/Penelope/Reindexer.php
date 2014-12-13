4c8e69fb12c4e0711646c79527adb498138b0cc0
Matthew Caruana Galizia <mattcg@gmail.com>
2014-12-13 15:08:46 -0600

    Add reindexing script

---
+++ scripts/Penelope/Reindexer.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * LICENSE: This source code is subject to the license that is available
+ * in the LICENSE file distributed along with this package.
+ *
+ * @package    Penelope
+ * @author     Matthew Caruana Galizia <mcg@karwana.com>
+ * @copyright  Karwana Ltd
+ * @since      File available since Release 1.0.0
+ */
+
+namespace Karwana\Penelope\Scripts;
+
+use Karwana\Penelope\Schema;
+use Karwana\Penelope\NodeSchema;
+
+class Reindexer {
+
+	public function reindex(Schema $schema, $batch = 10) {
+		foreach ($schema->getNodes() as $node_schema) {
+			$this->reindexSchema($node_schema, $batch);
+		}
+	}
+
+	public function reindexSchema(NodeSchema $node_schema, $batch) {
+		$skip = 0;
+
+		while (!empty($collection = $node_schema->getCollection(null, $skip, $batch))) {
+			$this->reindexCollection($collection);
+
+			$skip += $batch;
+		}
+	}
+
+	public function reindexCollection(array $nodes) {
+		try {
+			foreach ($nodes as $node) {
+				$node->index();
+			}
+		} catch (\Exception $e) {
+			throw new \Exception('Error while indexing ' . $node->getSchema()->getName() . ' node ' . $node->getId() . '.', 0, $e);
+		}
+	}
+}
---
+++ scripts/reindex.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * LICENSE: This source code is subject to the license that is available
+ * in the LICENSE file distributed along with this package.
+ *
+ * @package    Penelope
+ * @author     Matthew Caruana Galizia <mcg@karwana.com>
+ * @copyright  Karwana Ltd
+ * @since      File available since Release 1.0.0
+ */
+
+use Karwana\Penelope\Schema;
+use Karwana\Penelope\Scripts\Reindexer;
+
+function reindex(Schema $schema) {
+	$reindexer = new Reindexer();
+	$reindexer->reindex($schema);
+}
+
+if (empty($argv[1])) {
+	throw new \InvalidArgumentException('Missing initialization script path.');
+}
+
+require_once __DIR__ . '/Penelope/Reindexer.php';
+require_once $argv[1];
+
+if (!isset($schema)) {
+	$schema = $penelope->getSchema();
+}
+
+reindex($schema);
