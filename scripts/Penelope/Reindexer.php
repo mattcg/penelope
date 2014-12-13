45f4e86aaf368cef34303d65b9eb7e6e2267d4dc
Matthew Caruana Galizia <mattcg@gmail.com>
2014-12-13 15:19:15 -0600

    Sleep for half a second by default in between batches

--- scripts/Penelope/Reindexer.php
+++ scripts/Penelope/Reindexer.php
@@ -17,17 +17,18 @@ use Karwana\Penelope\NodeSchema;
 
 class Reindexer {
 
-	public function reindex(Schema $schema, $batch = 10) {
+	public function reindex(Schema $schema, $batch = 10, $usleep = 500000) {
 		foreach ($schema->getNodes() as $node_schema) {
-			$this->reindexSchema($node_schema, $batch);
+			$this->reindexSchema($node_schema, $batch, $usleep);
 		}
 	}
 
-	public function reindexSchema(NodeSchema $node_schema, $batch) {
+	public function reindexSchema(NodeSchema $node_schema, $batch, $usleep) {
 		$skip = 0;
 
 		while (!empty($collection = $node_schema->getCollection(null, $skip, $batch))) {
 			$this->reindexCollection($collection);
+			usleep($usleep);
 
 			$skip += $batch;
 		}
